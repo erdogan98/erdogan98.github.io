9d2d619c3f431b4d4f6aa4d4fce0c687abde1962
erdogan98 <ekervanli@outlook.com>
2025-12-20 16:30:28 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/",
+  plugins: [react(), tsconfigPaths()],
 });
